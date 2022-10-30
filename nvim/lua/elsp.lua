98f127e65e41fcb6f183a3647f86924eb186c6af
Arjun-Anubis <anubiz512z@gmail.com>
2022-10-30 19:39:32 +0530

    Changed update capabilities as per nvim_cmp_lsp

--- nvim/lua/elsp.lua
+++ nvim/lua/elsp.lua
@@ -97,4 +97,4 @@ cmp.setup({
    }),
 })                                                                                                                                                                               
 local capabilities = vim.lsp.protocol.make_client_capabilities()                                                                                                                        
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities) 
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities) 
