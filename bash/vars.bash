8f669144ad60e9996751b1c7e5559f665ba5d1c7
Arjun-Anubis <anubiz512z@gmail.com>
2022-10-31 19:48:29 +0530

    Fixed: exa, icon spacing

--- bash/vars.bash
+++ bash/vars.bash
@@ -8,3 +8,5 @@ export WASMTIME_HOME="$HOME/.wasmtime"
 export XDG_CONFIG_HOME=/home/anubi/.config
 export XDG_SCREENSHOTS_DIR=~/pix/ss
 export XDG_PICTURES_DIR=~/pix/nss
+
+export EXA_ICON_SPACING=2
